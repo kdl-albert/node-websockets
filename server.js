658508d20ccead1df5c1f7bdc0b84917dc1fbd6e
kdl-albert <32890718+kdl-albert@users.noreply.github.com>
2018-02-02 16:50:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('Example app listening on port 3000.');
 });
