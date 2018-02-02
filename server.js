5688e5781c325b2cb2f62b4da432c716e9ed61b4
kdl-albert <32890718+kdl-albert@users.noreply.github.com>
2018-02-02 16:48:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,38 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express();
-server.get('/', function (req, res) {
-	res.sendfile(INDEX);
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server');
 });
 
-server.get("/api/", function(req, res) {
-	wss.clients.forEach((client) => {
-	res.send('<b>Hello</b> welcome to my http server made with express');
-  });
- });
+// 1) Add a route that answers to all request types
+app.route('/article')
+.get(function(req, res) {
+    res.send('Get the article');
+})
+.post(function(req, res) {
+    res.send('Add an article');
+})
+.put(function(req, res) {
+    res.send('Update the article');
+});
 
-  server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// 2) Use a wildcard for a route
+// answers to : theANYman, thebatman, thesuperman
+app.get('/the*man', function(req, res) {
+    res.send('the*man');
+});
 
-const wss = new SocketServer({ server });
+// 3) Use regular expressions in routes
+// responds to : batmobile, batwing, batcave, batarang
+app.get(/bat/, function(req, res) {
+    res.send('/bat/');
+});
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000.');
 });
